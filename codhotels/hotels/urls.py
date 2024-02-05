487d2bf5a81b36131ccf651487225ea159b5009b
Aniket <1singlaaniket@gmail.com>
2024-02-05 08:51:04 +0530

    Added basic example for understanding templates

--- codhotels/hotels/urls.py
+++ codhotels/hotels/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from hotels import views
 
 urlpatterns = [
-    path("test/", views.test_func)
+    path("test/", views.test_func),
+    path("html/", views.html_view),
+    path("template/", views.template_view),
 ]
--- codhotels/hotels/views.py
+++ codhotels/hotels/views.py
@@ -1,7 +1,17 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # Create your views here.
 
 def test_func(request):
     return HttpResponse("TEST")
+
+
+def html_view(request):
+    return HttpResponse("<h1> This is Heading </h1>")
+
+
+def template_view(request):
+    return render(request, "hotels/template.html")
+
