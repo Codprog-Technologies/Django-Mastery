f6f913f0e7b8bb92ab8825a97e7d9db079c6d9e5
Aniket <1singlaaniket@gmail.com>
2024-02-08 09:15:32 +0530

    Demo'd Template Inheritance and url tags

--- codhotels/hotels/urls.py
+++ codhotels/hotels/urls.py
@@ -5,7 +5,7 @@ from hotels import views
 urlpatterns = [
     path("test/", views.test_func),
     path("html/", views.html_view),
-    path("template/", views.template_view),
-    path("home/", views.home),
-    path("about/", views.about_us),
+    path("list/", views.template_view, name="hotels_list"),
+    path("home/", views.home, name="home"),
+    path("about/", views.about_us, name="about"),
 ]
