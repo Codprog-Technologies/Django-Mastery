5aba4427b0c80f603e3823e1e252262cc706bc38
Aniket <1singlaaniket@gmail.com>
2024-02-29 07:34:41 +0530

    Explained how to register model to Django Admin and also explained verbose_name attribute on Field

--- codlegal/appointments/admin.py
+++ codlegal/appointments/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from appointments import models
+
+
 # Register your models here.
+
+# admin.site.register(models.PracticeArea)
+
+@admin.register(models.PracticeArea)
+class PracticeAreaAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ("Name", {"fields": ("name", )}),
+        ("Description", {"fields": ("description", )}),
+    )
