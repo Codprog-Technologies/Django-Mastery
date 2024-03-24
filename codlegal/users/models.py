15604f51a44a76ed2d42ce55c005026f0f40723b
Aniket <1singlaaniket@gmail.com>
2024-03-24 22:06:14 +0530

    Explained how to use email as username field for authentication and how to define custom usermanager

--- codlegal/users/models.py
+++ codlegal/users/models.py
@@ -1,10 +1,44 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 # Create your models here.
+class CustomUserManager(UserManager):
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Create and save a user with the given email, and password.
+        """
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
+
 
 class User(AbstractUser):
+    username = None
+    email = models.EmailField(_("email address"), blank=False, unique=True)
     dob = models.DateField(null=True, verbose_name="Date of Birth")
 
-    REQUIRED_FIELDS = ["email", "dob"]
+    REQUIRED_FIELDS = ["dob"]
+    USERNAME_FIELD = "email"
+
+    objects = CustomUserManager()
