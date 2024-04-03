86663c4bde71ed347657b363c92224c6d847badd
Aniket <1singlaaniket@gmail.com>
2024-04-03 09:26:01 +0530

    Explained how to do POST request using HTML form and how to handle POST Request in View. Also used authenticate and login functions from auth app

--- codlegal/users/views.py
+++ codlegal/users/views.py
@@ -1,3 +1,21 @@
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def login_view(request):
+    if request.method == "POST":
+        # 1. validate email + password valid -> authenticate
+        # 2. if valid -> login -> login
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request=request, username=email, password=password)
+        if user:
+            login(request, user)
+            return HttpResponse('Login successful')
+        else:
+            return HttpResponse('Invalid Username or Password')
+    else:
+        return render(request, "users/login.html")
