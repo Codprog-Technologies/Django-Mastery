dab48538bf4fe5ec667d2f58e6f2df6dec059bf7
Aniket <1singlaaniket@gmail.com>
2024-04-07 12:44:06 +0530

    Explained how to use Django Form class to create forms and explained benefits

--- codlegal/users/urls.py
+++ codlegal/users/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from users import views
 
 urlpatterns = [
-    path("login/", views.login_view, name="login")
+    path("login/", views.login_view_django_form, name="login")
 ]
