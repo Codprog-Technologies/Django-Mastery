83f89d491aa3b41c2c0b6a747bedacc4ea80b28c
Aniket <1singlaaniket@gmail.com>
2024-04-08 10:25:09 +0530

    Explained field clean methods and validators for field validation

--- codlegal/users/forms.py
+++ codlegal/users/forms.py
@@ -1,10 +1,19 @@
 from django import forms
 from django.contrib.auth import authenticate
+from django.core import validators
 
 
 class UserAuthenticationForm(forms.Form):
     email = forms.EmailField(widget=forms.EmailInput(attrs={"class": "form-control"}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={"class": "form-control"}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={"class": "form-control"}),
+                               validators=[validators.MinLengthValidator(2), validators.MaxLengthValidator(10)])
+
+    def clean_email(self):
+        # validation
+        # transformation
+        email = self.cleaned_data.get('email')
+        # raise forms.ValidationError('Email Is Invalid')
+        return email.lower()
 
     def clean(self):
         cleaned_data = self.cleaned_data
