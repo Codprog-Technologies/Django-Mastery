0e85c189d46487741a2338606804ab5d2d10d44d
Aniket <1singlaaniket@gmail.com>
2024-04-13 23:36:17 +0530

    Explained how to create form using ModelForm class. Example: PracticeArea form. Also shown a hacky way to include bootstrap css class for all visible fields.

--- codlegal/appointments/urls.py
+++ codlegal/appointments/urls.py
@@ -5,4 +5,5 @@ from appointments import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
+    path("practice-area/", views.practice_area, name="practice_area"),
 ]
