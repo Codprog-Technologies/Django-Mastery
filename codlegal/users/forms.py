17465c300479806bf5d17978278624ef5ad0b4c7
Aniket <1singlaaniket@gmail.com>
2024-04-14 20:03:35 +0530

    Explained how to create user signup form and also formatted navigation bar to hide login and signup links if user is logged in.

--- codlegal/users/forms.py
+++ codlegal/users/forms.py
@@ -1,6 +1,9 @@
 from django import forms
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, get_user_model
+from django.contrib.auth.forms import BaseUserCreationForm
 from django.core import validators
+
+from users import models
 
 
 class UserAuthenticationForm(forms.Form):
@@ -28,3 +31,20 @@ class UserAuthenticationForm(forms.Form):
 
     def get_user(self):
         return self.user
+
+
+class UserSignupForm(BaseUserCreationForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for bound_field in self.visible_fields():
+            if bound_field.field.widget.attrs.get('class'):
+                bound_field.field.widget.attrs['class'] += ' form-control'
+            else:
+                bound_field.field.widget.attrs['class'] = 'form-control'
+
+    class Meta:
+        # model = models.User # get_user_model()
+        model = get_user_model()
+        fields = ("email", "first_name", "last_name", "dob")
+        # exclude = ("last_login", "password", "date_joined")
