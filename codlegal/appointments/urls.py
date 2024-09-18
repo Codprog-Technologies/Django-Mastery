daa399f63eff8f9a9db9544ef30bc3374e2e28bd
Aniket <1singlaaniket@gmail.com>
2024-09-18 08:37:29 +0530

    Demo'd DeleteView with help of appointment delete view

--- codlegal/appointments/urls.py
+++ codlegal/appointments/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("practice-area/", views.PracticeAreaView.as_view(), name="practice_area"),
     path("appointments/book/", views.AppointmentCreateView.as_view(), name="appointment_create"),
     path("appointments/list/", views.UpcomingAppointmentListView.as_view(), name="appointment_list"),
+    path("appointments/<int:pk>/delete/", views.AppointmentDeleteView.as_view(), name="appointment_delete"),
 ]
