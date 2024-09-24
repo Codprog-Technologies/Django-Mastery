10af03cbf4bce4b488c7c819bd19a9b4175db8f3
Aniket <1singlaaniket@gmail.com>
2024-09-24 08:36:07 +0530

    Explained how to use PasswordChangeViw and PasswordChangeDoneView for change password flow

--- codlegal/users/urls.py
+++ codlegal/users/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, PasswordChangeView, PasswordChangeDoneView
 from django.urls import path
 
 from users import views
@@ -6,6 +6,8 @@ from users import views
 urlpatterns = [
     path("login/", LoginView.as_view(template_name="users/login_with_django_form.html"), name="login"),
     path("logout/", views.logout_view, name="logout"),
+    path("password-change/", PasswordChangeView.as_view(template_name="users/password_change_form.html"), name="password_change"),
+    path("password-change-done/", PasswordChangeDoneView.as_view(template_name="users/password_change_done.html"), name="password_change_done"),
     # path("signup/", views.signup, name="signup"),
     path("signup/advocate/", views.advocate_signup, name="advocate_signup"),
     path("signup/client/", views.client_signup, name="client_signup"),
