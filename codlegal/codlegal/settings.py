26de46f6426fc6cbc1c33991d27d97c9e05a8b67
Aniket <1singlaaniket@gmail.com>
2024-11-16 18:19:11 +0530

    Explained how to use LocaleMiddleware to dynamically translate Pages based on User Provided locale in request

--- codlegal/codlegal/settings.py
+++ codlegal/codlegal/settings.py
@@ -116,6 +116,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -207,7 +208,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "hi"
+LANGUAGE_CODE = "en"
 
 TIME_ZONE = "UTC"
 
