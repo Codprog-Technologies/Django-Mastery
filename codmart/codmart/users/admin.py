26edadc6b86859a71713b4e6456336fb2eb3a272
Aniket <1singlaaniket@gmail.com>
2025-01-12 01:46:52 +0530

    Define custom user model and add the same to Django Admin.

---
+++ codmart/codmart/users/admin.py
@@ -0,0 +1,40 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext as _
+
+from users import models
+
+
+# Register your models here.
+
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("password", )}),
+        (_("Personal info"), {"fields": ("username", "first_name", "last_name", "email", "role")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined", "dob")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
+    list_display = ("email", "first_name", "last_name", "is_staff")
+    search_fields = ("first_name", "last_name", "email")
+    ordering = ("pk",)
