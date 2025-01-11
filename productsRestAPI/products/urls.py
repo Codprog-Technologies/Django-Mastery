5d6a7ab7522171c3edbc0682a5bca97d74faf51d
Aniket <1singlaaniket@gmail.com>
2025-01-11 22:49:10 +0530

    Demo for creating api with help of Django Core Serializers

--- productsRestAPI/products/urls.py
+++ productsRestAPI/products/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 
 from products import views
+from products.views import save_product_api_django_core
 
 router = SimpleRouter()
 
@@ -9,4 +10,5 @@ router.register('products', views.ProductViewSet, basename='products')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('django-create-product/', save_product_api_django_core, name="django-create-product")
 ]
--- productsRestAPI/products/views.py
+++ productsRestAPI/products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.core import serializers as django_serializers
+from django.http import JsonResponse, HttpResponseNotAllowed, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.viewsets import ModelViewSet
 
 from products import models, serializers
@@ -8,5 +10,26 @@ from products import models, serializers
 
 
 class ProductViewSet(ModelViewSet):
-    queryset = models.Products.objects.all()
+    queryset = models.Product.objects.all()
     serializer_class = serializers.ProductsSerializer
+
+
+@csrf_exempt
+def save_product_api_django_core(request):
+    """
+    Save Product Information ( Core Django )
+    """
+    if request.method != 'POST':
+        return HttpResponseNotAllowed(['POST'])
+    if request.META.get('CONTENT_TYPE') != 'application/json':
+        return JsonResponse({'detail': 'Content-Type not supported'}, status=415)
+    deserialized_data = django_serializers.deserialize('json', request.body)
+    saved_instances = []
+    for data in deserialized_data:
+        data.save()
+        saved_instances.append(data.object)
+    return HttpResponse(django_serializers.serialize(
+        'json', saved_instances,
+        fields=['id', 'name', 'price']),
+        content_type='application/json'
+    )
