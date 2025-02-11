6f1106188d01d60bc95cefbfed3753c5c08438d2
Aniket <1singlaaniket@gmail.com>
2025-02-11 08:22:46 +0530

    Created first function-based DRF view using @api_view decorator

--- codmart/codmart/users/views.py
+++ codmart/codmart/users/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from users import models
+
 
 # Create your views here.
+
+@api_view(['POST'])
+def create_user_api(request):
+    # request.method / request.POST
+    # request.data
+    # return Response()
+    if request.method == 'POST':
+        user = models.User.objects.create_user(**request.data)
+        request.data['id'] = user.pk
+        request.data.pop('password')
+        return Response(request.data, status=status.HTTP_201_CREATED)
