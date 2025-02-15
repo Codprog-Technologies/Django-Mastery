41425fa83bff18d4baf7e9a6c7d1b9ed748f9de9
Aniket <1singlaaniket@gmail.com>
2025-02-15 12:29:02 +0530

    Explained how to use serializer to Deserialize and Serialize data in view (UserSerializer)

--- codmart/codmart/users/views.py
+++ codmart/codmart/users/views.py
@@ -3,7 +3,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from users import models
+from users import models, serializers
 
 
 # Create your views here.
@@ -14,7 +14,15 @@ def create_user_api(request):
     # request.data
     # return Response()
     if request.method == 'POST':
-        user = models.User.objects.create_user(**request.data)
-        request.data['id'] = user.pk
-        request.data.pop('password')
-        return Response(request.data, status=status.HTTP_201_CREATED)
+        # user = models.User.objects.create_user(**request.data)
+        # request.data['id'] = user.pk
+        # request.data.pop('password')
+        # return Response(request.data, status=status.HTTP_201_CREATED)
+        # deserialization and validation -> request.data -> user
+        # serialization -> user -> dict
+        serializer = serializers.UserSerializer(data=request.data) # deserialization and validation
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED) # serialization
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
