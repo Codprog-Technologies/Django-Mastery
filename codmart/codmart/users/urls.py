95e0e2267672954409200087200c3dddb65e5851
Aniket <1singlaaniket@gmail.com>
2025-02-24 22:37:03 +0530

    Explain Generic Views: CreateApiView and DestroyApiView. Also update user serializer to mark some fields as read_only or remove them

--- codmart/codmart/users/urls.py
+++ codmart/codmart/users/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from users import views
 
 urlpatterns = [
-    path('', views.create_user_api, name="create_user")
+    path('', views.CreateUserView.as_view(), name="create_user"),
+    path('<int:pk>/', views.DestroyUserView.as_view(), name="destroy_user")
 ]
