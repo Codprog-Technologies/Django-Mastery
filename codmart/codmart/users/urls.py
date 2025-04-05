e1e5a56e5379f6b8d715eb8bc5d03dd337d220f1
Aniket <1singlaaniket@gmail.com>
2025-04-05 18:17:14 +0530

    Explained how to use multiple http methods at same url route with help of combination generic views

--- codmart/codmart/users/urls.py
+++ codmart/codmart/users/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from users import views
 
 urlpatterns = [
-    path('', views.CreateUserView.as_view(), name="create_user"),
+    # path('', views.CreateUserView.as_view(), name="create_user"),
+    path('', views.ListCreateUserView.as_view(), name="list_create_user"),
     # path('<int:pk>/', views.DestroyUserView.as_view(), name="destroy_user"),
-    path('<int:pk>/', views.UpdateUserView.as_view(), name="update_user")
+    # path('<int:pk>/', views.UpdateUserView.as_view(), name="update_user")
+    path('<int:pk>/', views.RetrieveUpdateDestroyUserView.as_view(), name="retrieve_update_destroy_user")
 ]
