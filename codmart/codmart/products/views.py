3e205ec5934a24fd5291dd0764b1c357ca84c195
Aniket <1singlaaniket@gmail.com>
2025-04-13 15:58:25 +0530

    Explained how to contain all endpoints in single CBV with help of ViewSets and Routers

--- codmart/codmart/products/views.py
+++ codmart/codmart/products/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+
+from products import models, serializers
+
 
 # Create your views here.
+
+class ProductViewSet(viewsets.ModelViewSet):  # Or Inherit from GenericViewSet along with mixins
+
+    queryset = models.Product.objects.all()
+    serializer_class = serializers.ProductSerializer
