2475ffddf86c37fa3db02f952b672ad79a05459d
Aniket <1singlaaniket@gmail.com>
2025-04-15 22:27:43 +0530

    Explained Nested Serializers

--- codmart/codmart/products/models.py
+++ codmart/codmart/products/models.py
@@ -7,6 +7,7 @@ class Product(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
+    # productimage_set
 
 
 class ProductImage(models.Model):
--- codmart/codmart/products/serializers.py
+++ codmart/codmart/products/serializers.py
@@ -3,8 +3,31 @@ from rest_framework import serializers
 from products import models
 
 
+class ProductImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.ProductImage
+        fields = ('url', 'id')
+
+
 class ProductSerializer(serializers.ModelSerializer):
+    images = ProductImageSerializer(many=True, source='productimage_set', required=True)
 
     class Meta:
         model = models.Product
         fields = '__all__'
+
+    def create(self, validated_data):
+        images = validated_data.pop('productimage_set')
+        product = super().create(validated_data)
+        for image in images:
+            product.productimage_set.create(**image)
+        return product
+
+    def update(self, instance, validated_data):
+        if validated_data.get('productimage_set'):
+            images = validated_data.pop('productimage_set')
+            instance.productimage_set.all().delete()
+            for image in images:
+                instance.productimage_set.create(**image)
+
+        return super().update(instance, validated_data)
