eb583259a3ef06b7aa38465230c7fdcb77672faf
Aniket <1singlaaniket@gmail.com>
2025-05-26 23:33:46 +0530

    Explain custom permission classes and how to use them at view level

--- codmart/codmart/codmart/settings.py
+++ codmart/codmart/codmart/settings.py
@@ -69,7 +69,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1)
 }
 
---
+++ codmart/codmart/users/permissions.py
@@ -0,0 +1,14 @@
+from rest_framework import permissions
+
+
+class IsOwner(permissions.BasePermission):
+    """
+    Object-level permission to only allow owners of an object to edit it.
+    Assumes the model instance has an `owner` attribute.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+
+        # Instance must have an attribute named `owner`.
+        return obj == request.user
