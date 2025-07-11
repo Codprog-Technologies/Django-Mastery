3a2aa1f7ecb1c0063340eab6ac164627e58e93f2
Aniket <1singlaaniket@gmail.com>
2025-07-11 19:18:21 +0530

    Started Payments app and models required for Order creation

--- codmart/codmart/codmart/settings.py
+++ codmart/codmart/codmart/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'users',
     'products',
+    'payments',
     'rest_framework',
     'rest_framework.authtoken',
     'rest_framework_simplejwt.token_blacklist',
--- codmart/codmart/users/models.py
+++ codmart/codmart/users/models.py
@@ -1,4 +1,6 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
+from django.core import validators
 from django.db import models
 from django.utils.translation import gettext as _
 
@@ -17,3 +19,14 @@ class User(AbstractUser):
 
     REQUIRED_FIELDS = ["email", "dob"]
     # USERNAME_FIELD = "email"
+
+
+class Address(models.Model):
+    name = models.CharField(max_length=30)
+    state = models.CharField(max_length=20)
+    address_line_1 = models.CharField(max_length=50)
+    address_line_2 = models.CharField(max_length=50)
+    pincode = models.CharField(validators=[validators.MinLengthValidator(6),
+                                           validators.MaxLengthValidator(6)])
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    phone_number = models.CharField(validators=[validators.RegexValidator(r'^\d{10}$')])
