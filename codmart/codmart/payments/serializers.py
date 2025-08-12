b1c917a9f663074570f8b0a8e6061334c60a01de
Aniket <1singlaaniket@gmail.com>
2025-08-12 08:19:30 +0530

    Explained how to use Stripe SDK to create Payment Intent Object in our Django Project

--- codmart/codmart/payments/serializers.py
+++ codmart/codmart/payments/serializers.py
@@ -9,11 +9,22 @@ class OrderCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Order
         fields = "__all__"
-        read_only_fields = ('user', 'total_amount', 'status', 'created_at', 'updated_at')
+        read_only_fields = ('user', 'total_amount', 'status', 'pg_id', 'created_at', 'updated_at')
+        extra_kwargs = {
+            'product': {'required': True, 'allow_null': False},
+            'address': {'required': True, 'allow_null': False}
+        }
+
+
+    def validate(self, attrs):
+        product = attrs.get('product')
+        gst_amount = (product.price * 18) / 100
+        attrs['total_amount'] = product.price + gst_amount
+        if attrs['payment_channel'] == models.PaymentChannel.ONLINE:
+            attrs['status'] = models.OrderStatus.CREATED
+        else:
+            attrs['status'] = models.OrderStatus.CONFIRMED
+        return attrs
 
     def create(self, validated_data):
-        product = validated_data.get('product')
-        gst_amount = (product.price * 18) / 100
-        validated_data['total_amount'] = product.price + gst_amount
-        validated_data['status'] = models.OrderStatus.CONFIRMED
         return super().create(validated_data)
